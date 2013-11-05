b291abdb344b98122509e608f15c3968b83b0f61
Itch84 <Itch84@gmail.com>
2013-11-05 14:58:38 +0100

    Uebung 4 fertiggestellt

--- app/controllers/fizz_controller.rb
+++ app/controllers/fizz_controller.rb
@@ -1,9 +1,32 @@
 class FizzController < ApplicationController
+    
+   
+    
   def Buzz
-    @fizz_name = "Fizz"
-    @buzz_name = "Buzz"
+    @fizz_name = "Izzy"
+    @buzz_name = "Bizzy"
     @fizz_number = 3
-    @bizz_number = 5
+    @buzz_number = 5
     @max_number = 100
   end
+  
+  def check(x)
+    
+    if x%3 == 0 && x%5==0
+      puts "Fizzbuzz"
+    elsif x%3 == 0
+      puts "Fizz"
+    elsif x%5 == 0
+      puts "Buzz"
+    else
+      puts x
+    end
+    
+    
+    #(x%3 != 0 && x%5 != 0) ? x : ""
+    #puts "Fizz" if x%3 == 0
+    #puts "Buzz" if x%5 == 0
+    #puts "FizzBuzz" if (x%3 == 0 && x%5==0) 
+  end
+  
 end
--- app/helpers/fizz_helper.rb
+++ app/helpers/fizz_helper.rb
@@ -1,2 +1,22 @@
 module FizzHelper
+  
+    def fizzy(num)
+    
+     if num%3 == 0 && num%5==0
+        return @fizz_name + @buzz_name
+      elsif num%3 == 0
+       return @fizz_name
+      elsif num%5 == 0
+        return @buzz_name
+      else
+       return num
+      end
+    
+     end
+  
+    def color_chooser(num)
+    
+      (num%3 == 0 && num%5==0) ? "bgcolor=#fee000" : ""
+    end
+  
 end
